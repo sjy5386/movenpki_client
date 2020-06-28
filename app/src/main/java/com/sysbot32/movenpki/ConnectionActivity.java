4cdf47cfcf6d9590f7d16859e64668db792b0e0f
Seokjin Yoon <sjy5386@gmail.com>
2020-06-28 12:21:54 +0900

    Add menu_main

--- app/src/main/java/com/sysbot32/movenpki/ConnectionActivity.java
+++ app/src/main/java/com/sysbot32/movenpki/ConnectionActivity.java
@@ -1,10 +1,10 @@
 package com.sysbot32.movenpki;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.sysbot32.movenpki.databinding.ActivityConnectionBinding;
 
--- app/src/main/java/com/sysbot32/movenpki/MainActivity.java
+++ app/src/main/java/com/sysbot32/movenpki/MainActivity.java
@@ -1,10 +1,12 @@
 package com.sysbot32.movenpki;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.sysbot32.movenpki.databinding.ActivityMainBinding;
@@ -31,5 +33,21 @@ public class MainActivity extends AppCompatActivity {
         if (!client.isConnected()) {
             startActivity(new Intent(this, ConnectionActivity.class));
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menuItem) {
+            startActivity(new Intent(this, ConnectionActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
